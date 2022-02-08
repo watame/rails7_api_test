ac41d0a5ab22d050330cee0c3bda79ab008539c4
watame <pandora.border@gmail.com>
2022-02-08 21:59:39 +0900

    とりあえず通信できるところまで

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,12 @@
+# namespaceのネスト
+module Api
+  # namespaceのネスト
+  module V1
+    class PostsController < ApplicationController
+      def create
+        post = Posts.new(title: "test_title", description: "test_description")
+        render json: { status: 'SUCCESS', data: post }
+      end
+    end
+  end
+end
---
+++ app/models/concerns/posts.rb
@@ -0,0 +1,5 @@
+class Posts
+  include ActiveModel::Model
+  attr_accessor :title,
+                :description
+end
