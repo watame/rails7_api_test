eb2b58676cf56d609e808f5da620062f1b56deec
watame <pandora.border@gmail.com>
2022-02-15 23:05:10 +0900

    :+1: #2 swagger-uiのexecuteを行うため、rack-corsを導入

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    if Rails.env.development?
+      # swagger-ui コンテナからの通信を許可
+      origins "localhost:8080"
+      resource "*",
+        headers: :any,
+        methods: :any
+    end
+  end
+end
