25ee3e07ce2c509463900c9f51c2bf3c01d16134
watame <pandora.border@gmail.com>
2022-02-20 21:53:33 +0900

    :bug: #2 rack-corsがRSpecで読み込めないエラーを改修

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,7 @@
+# RSpec実行時に`uninitialized constant Rack::Cora`エラーが出るため、明示的にrequireを行う
+# https://github-com.translate.goog/cyu/rack-cors/issues/26?_x_tr_sl=auto&_x_tr_tl=ja&_x_tr_hl=ja&_x_tr_pto=op,wapp#issuecomment-100170167
+require 'rack'
+require 'rack/cors'
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -11,8 +15,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       # swagger-ui コンテナからの通信を許可
       origins "localhost:8080"
       resource "*",
+        :headers => :any,
+        :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+    if Rails.env.test?
+      origins '*'
+      resource '*',
         headers: :any,
-        methods: :any
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
 end
