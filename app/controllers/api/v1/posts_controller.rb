a6d31ec4b0612566ea93ab1bfbc0833595e242c9
watame <pandora.border@gmail.com>
2022-03-07 22:19:17 +0900

    add strong parameter method

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,8 +4,18 @@ module Api
   module V1
     class PostsController < ApplicationController
       def create
-        post = Post.new(title: "test_title", description: "test_description")
-        render json: { status: 'SUCCESS', data: post }
+        # 受け取るパラメータはRailsの仕様で以下のように改変される
+        #   送信内容: { "title": "test title", "description": "test description" }
+        #   params: {"title"=>"test title", "description"=>"test description", "post"=>{"title"=>"test title", "description"=>"test description"}}
+        # https://railsguides.jp/action_controller_overview.html#json%E3%83%91%E3%83%A9%E3%83%A1%E3%83%BC%E3%82%BF
+        post = Post.new(post_params)
+        render json: post, status: :ok
+      end
+      
+      private
+
+      def post_params
+        params.require(:post).permit(:title, :description)
       end
     end
   end
