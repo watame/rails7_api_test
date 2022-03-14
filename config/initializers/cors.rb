c0cbe20fb1ed6d6308e6bd9419f1015fa2b53bdb
watame <pandora.border@gmail.com>
2022-03-14 22:20:35 +0900

    :+1: #4 fix Style/SymbolArray

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,13 +16,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "localhost:8080"
       resource "*",
                headers: :any,
-               methods: [:get, :post, :put, :patch, :delete, :options, :head]
+               methods: %i[get post put patch delete options head]
     end
     if Rails.env.test?
       origins "*"
       resource "*",
                headers: :any,
-               methods: [:get, :post, :put, :patch, :delete, :options, :head]
+               methods: %i[get post put patch delete options head]
     end
   end
 end
