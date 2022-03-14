554f173758ba3cedab52e7b2565c6f9812e04472
watame <pandora.border@gmail.com>
2022-03-14 22:21:36 +0900

    :+1: #4 fix Style/TrailingCommaInHashLiteral

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ RSpec.configure do |config|
   config.committee_options = {
     schema_path: Rails.root.join("doc/openapi.yml").to_s,
     query_hash_key: "rack.request.query_hash",
-    parse_response_by_content_type: false,
+    parse_response_by_content_type: false
   }
   config.include Committee::Rails::Test::Methods
 
